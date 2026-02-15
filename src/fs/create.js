7b0b8a2b62ab9afb8bd4328d49a48f2243321661
Nikita <nktvrntsv@gmail.com>
2026-02-15 23:32:02 +0300

    feat/(base-node-js): Изучил базу Node js, Написал первую функцию Create
    Изучил встроенные функции Node js
    - fs - отвечает за файлы
    - path - отвечает за пути
    - url - отвечает за преобразование url в путь

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { writeFile, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const fPath = join(__dirname, 'files', 'frsh.txt');
+
+    try {
+        await access(fPath);
+        throw new Error('Операция гг');
+    } catch (err) {
+        if (err.message === 'Операция гг') {
+            throw err;
+        }
+        await writeFile(fPath, 'I am fresh and young');
+    }
 };
 
 await create();
