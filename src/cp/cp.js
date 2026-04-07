ce2e22360f1c444cf4d39f47501bcf50536deede
Nikita Vorontsov <nktvrntsv@gmail.com>
2026-04-07 09:12:31 +0300

    feat/(base-node-js): Поправил имя файла

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const spawnChildProcess = async (args) => {
-    const scriptPath = join(__dirname, 'script.js');
+    const scriptPath = join(__dirname, 'files', 'script.js');
 
     const child = spawn('node', [scriptPath, ...args], {
         stdio: ['pipe', 'pipe', 'inherit']
